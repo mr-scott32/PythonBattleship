60f916bff23913c203ffaff1a5d3fae29bb18ab0
mr-scott32 <128337536+mr-scott32@users.noreply.github.com>
2026-09-17 15:16:57 +1000

    somewhat working things

---
+++ main.py
@@ -0,0 +1,133 @@
+import random
+def create_board():
+    grid = []
+    for i in range(10):
+        row = []
+        for j in range(10):
+            row.append('-')
+
+        grid.append(row)
+    return grid
+
+
+def setup_player():
+
+    grid = create_board()
+
+    print('Carrier: 5 ' \
+    'Battleship: 4 ' \
+    'Cruiser: 3 ' \
+    'Submarine: 3 ' \
+    'Destroyer: 2 ')
+
+    ships = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine':3, 'Destroyer':2 }
+    
+
+
+    for k, v in ships.items():
+        placement = False
+        print(f'Placing a {k}. This will take {v} spaces.')
+        orient = int(input(f'Place your {k}. ' \
+            'Enter "1" for Horizontal or "2" for Vertical: '))
+        if orient == 1:
+            while placement == False:
+                y_pos = int(input('Enter which COLUMN from 0-9 your ship will be placed on. It will be placed rightward from here: '))
+                x_pos = int(input('Enter which ROW from 0-9 your ship will be placed on: '))
+                for i in range(v):
+                    if grid[x_pos][y_pos+i] == 'S' or y_pos > 9:
+                        print('Invalid placement!')
+                        break
+                    else:
+                        for i in range(v):
+                            grid[x_pos][y_pos+i] = 'S'
+                            placement = True
+
+        elif orient == 2:
+            while placement == False:
+                x_pos = int(input('Enter which ROW from 0-9 your ship will be placed on. It will be placed downward from here: '))
+                y_pos = int(input('Enter which COLUMN from 0-9 your ship will be placed on: '))
+                for i in range(v):
+                    if grid[x_pos+i][y_pos] == 'S' or x_pos > 9:
+                        print('Invalid placement!')
+                        break
+                    else:
+                        for i in range(v):
+                            grid[x_pos+i][y_pos] = 'S'
+                            placement = True
+
+        
+        else:
+            print('Please enter a valid value')
+
+
+        for i in range(10):
+            for j in range(10):
+                print(grid[i][j], end =' ')
+            print()
+    return grid
+
+def setup_cpu():
+    grid = create_board()
+
+    ships = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine':3, 'Destroyer':2 }
+
+    for k, v in ships.items():
+        placement = False
+        print(f'Placing a {k}. This will take {v} spaces.')
+        orient = random.randint(1, 2)
+
+        if orient == 1:
+            while placement == False:
+                y_pos = random.randint(0, 10-v)
+                x_pos = random.randint(0, 9)
+                for i in range(v):
+                    if grid[x_pos][y_pos+i] == 'S':
+                        break
+                else:
+                    for i in range(v):
+                        grid[x_pos][y_pos+i] = 'S'
+                    placement = True
+
+        elif orient == 2:
+            while placement == False:
+                x_pos = random.randint(0, 10-v)
+                y_pos = random.randint(0, 9)
+
+                for i in range(v):
+                    if grid[x_pos+i][y_pos] == 'S':
+                        break
+                else:
+                    for i in range(v):
+                        grid[x_pos+i][y_pos] = 'S'
+                    placement = True
+
+    return grid
+
+def attack_grid():
+    battle_grid = create_board()
+    cpu_grid = setup_cpu()
+    turn = True
+    while turn == True:
+        for i in range(10):
+            for j in range(10):
+                print(battle_grid[i][j], end =' ')
+            print()
+        x_pos = int(input('Enter x coordinate from 0-9'))
+        y_pos = int(input('Enter y coordinate from 0-9'))
+
+        try:
+            if cpu_grid[x_pos][y_pos] == 'S' and battle_grid[x_pos][y_pos] != 'X' and battle_grid[x_pos][y_pos] != 'O':
+                print('Hit!')
+                battle_grid[x_pos][y_pos] = 'X'
+            else:
+                print('Miss!')
+                battle_grid[x_pos][y_pos] = 'O'
+        except:
+            print('Please land it on the board at least')
+    return battle_grid
+
+    
+
+#setup_player()
+setup_cpu()
+attack_grid()
